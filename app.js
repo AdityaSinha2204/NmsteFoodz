6a96e87752932a8039e3ba56601882f52ce0fea7
AdityaSinha2204 <as120360adityasinha@gmail.com>
2024-01-05 18:45:44 +0530

    food delivery app

---
+++ app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const parent= React.createElement("h1",{},"Iam headung");
+
+const root = ReactDOM.createroot(React.createElementById(root));
+ root.render(parent);
